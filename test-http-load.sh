3fbaa3861ecc7f0b367f4a3824ab40025446a4d1
brian avery <avery.brian@gmail.com>
2015-11-30 14:26:23 -0800

    1) exit if fail so we see it 2) add a random delay between 0 and .25 seconds 3) print out the count and the time of a failed transaction

--- test-http-load.sh
+++ test-http-load.sh
@@ -2,11 +2,20 @@ echo "Insert records from HTTP and a script to try to throw 'database is locked'
 
 # make num higher to add more records per request
 NUM=1
+COUNT=0
 
-# make SLEEP higher to pause between requests
-SLEEP=0
 
 while true ; do
-  curl "http://127.0.0.1:8000/overloader/?num=$NUM"
-  sleep $SLEEP
+    let COUNT=$COUNT+1
+    TIME_START=`python -c "import time;print int(round(time.time()))"`
+    OUTPUT=`curl -s "http://127.0.0.1:8000/overloader/?num=$NUM"`
+    TIME_DONE=`python -c "import time;print int(round(time.time()))"`
+    echo $OUTPUT "count=$COUNT"
+    if [ `echo $OUTPUT | wc -c` != 15 ]; then
+	let TIME_DELTA=$TIME_DONE-$TIME_START
+	echo "BAD RECORD INSERT on $COUNT attempt took " $TIME_DELTA
+	exit -1
+    fi
+    # we need some space to have access to db during backoff
+    sleep `python -c "import time;import random;random.seed(time.time());print random.random()*0.25"`
 done
