5877ea560e0fbd7f18840df87e483b6670d545e8
brian avery <avery.brian@gmail.com>
2015-11-30 14:25:43 -0800

    just delete the db to start a run.

--- test-run.sh
+++ test-run.sh
@@ -1,11 +1,11 @@
 reset
 
+echo "Clean out db..."
+rm -f  db.sqlite3
+
 echo "Applying migrations..."
 ./manage.py migrate
 
-echo "Clean out spurious records table..."
-sqlite3 db.sqlite3 "DELETE FROM overloader_overloaderfoo"
 
 echo "Start server..."
 ./manage.py runserver
-
